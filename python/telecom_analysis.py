a60f4b194a99b0175db6448f81c71850e9dcf2af
Aryan Awasthi <aryanawasthi344@gmail.com>
2026-05-04 08:48:44 +0530

    Update telecom_analysis.py

--- python/telecom_analysis.py
+++ python/telecom_analysis.py
@@ -34,3 +34,103 @@ plt.title("Churn Rate by Monthly Charges")
 plt.savefig("charge_churn.png")
 
 print("All charts saved successfully")
+df = pd.read_csv("telecom_customer_churn.csv")
+
+# 🔍 (Optional but useful)
+
+print("Columns:", df.columns)
+
+# ------------------------------
+
+# R = Recency (tenure)
+
+# ------------------------------
+
+df['R_score'] = pd.qcut(df['Tenure in Months'], 3, labels=[3,2,1])
+
+# ------------------------------
+
+# F = Frequency (contract)
+
+# ------------------------------
+
+def freq_map(x):
+
+    if x == "Month-to-month":
+
+        return 1
+
+    elif x == "One year":
+
+        return 2
+
+    else:
+
+        return 3
+
+df['F_score'] = df['Contract'].apply(freq_map)
+
+# ------------------------------
+
+# M = Monetary (monthly charge)
+
+# ------------------------------
+
+df['M_score'] = pd.qcut(df['Monthly Charge'], 3, labels=[1,2,3])
+
+# ------------------------------
+
+# Combine RFM
+
+# ------------------------------
+
+df['RFM_score'] = (
+
+    df['R_score'].astype(str) +
+
+    df['F_score'].astype(str) +
+
+    df['M_score'].astype(str)
+
+)
+
+# ==============================
+
+# 4. SEGMENTATION
+
+# ==============================
+
+def segment(row):
+
+    if row['RFM_score'] == "333":
+
+        return "Champion"
+
+    elif int(row['R_score']) == 1:
+
+        return "At Risk"
+
+    else:
+
+        return "Regular"
+
+df['segment'] = df.apply(segment, axis=1)
+
+# ==============================
+
+# 5. OUTPUT
+
+# ==============================
+
+print(df[['RFM_score', 'segment']].head())
+
+
+rfm_summary = df.groupby('segment').size().reset_index(name='customer_count')
+
+rfm_summary.to_csv("rfm_output.csv", index=False)
+
+print("RFM Analysis Completed ")
+
+rfm_summary = df.groupby('segment').size().reset_index(name='customer_count')
+
+print(rfm_summary)
